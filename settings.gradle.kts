5812dfedfdd286c63a16bbdd9f9d9d0fd54d177a
1fexd <PGFvy85nC@protonmail.com>
2024-06-22 19:26:44 +0200

    chore: Add gradle files

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+rootProject.name = "super"
+
+//pluginManagement {
+//    repositories {
+//        mavenLocal()
+//        maven(url = "https://jitpack.io")
+//        gradlePluginPortal()
+//    }
+//
+//    plugins {
+//        kotlin("jvm") version "1.9.23"
+//        id("de.fayard.refreshVersions") version "0.60.1"
+//        id("org.gradle.maven-publish")
+//        id("net.nemerosa.versioning") version "3.0.0"
+//    }
+//}
+//
+//plugins {
+//    kotlin("jvm") apply false
+//    id("de.fayard.refreshVersions") apply false
+//    id("net.nemerosa.versioning") apply false
+//}
+
+val excludes = setOf("gradle")
+
+rootProject.projectDir.listFiles()
+    ?.filter { it.isDirectory && !it.name.startsWith(".") && it.name !in excludes }
+    ?.forEach { includeBuild(it) }
